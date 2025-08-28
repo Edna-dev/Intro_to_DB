8cc162b140c2e0f0d4a4962bfb9192862f53694b
Edna Wangui <your-email@example.com>
2025-08-28 07:37:02 +0300

    Fix: insert multiple customers (task_6.sql) - exact values

--- task_6.sql
+++ task_6.sql
@@ -1,10 +1,17 @@
 USE alx_book_store;
 
-INSERT INTO customer (customer_id, customer_name, email, address)
-VALUES (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.');
+CREATE TABLE IF NOT EXISTS customer (
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(215),
+    email VARCHAR(215),
+    address TEXT
+);
 
 INSERT INTO customer (customer_id, customer_name, email, address)
-VALUES (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.');
+VALUES (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.');
 
 INSERT INTO customer (customer_id, customer_name, email, address)
-VALUES (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
+VALUES (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness  Ave.');
+
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUES (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness  Ave.');
